58bac2eaa7891e8adb3f42a3967b4c7fac7244ba
Vitalii Oliinyk <vitaliyoliynykk@gmail.com>
2018-10-12 09:55:07 +0000

    Smooth scroll

--- js/index.js
+++ js/index.js
@@ -11,3 +11,27 @@ function burgerMenu(e){
         x.className='topNav'
     }
 }
+$(document).ready(function() {
+    $('.smooth').click(function(e) {
+        e.preventDefault();
+        let linkHref = $(this).attr('href');
+        $('html,body').animate({
+            scrollTop: $(linkHref).offset().top
+        }, 850)
+    });
+
+    $(function() {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() >= 100) {
+                $('#comeback').fadeIn();
+            } else {
+                $('#comeback').fadeOut();
+            }
+        });
+        $('#comeback').click(function() {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+        });
+    });
+});
